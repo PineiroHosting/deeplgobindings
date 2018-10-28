b958bca7741016ef44839ced8893df103582c908
mmichaelb <mmichaelb@protonmail.com>
2018-10-29 00:50:57 +0100

    Add function to execute API requests

--- pkg/client.go
+++ pkg/client.go
@@ -1,11 +1,14 @@
 package deeplgobindings
 
 import (
+	"fmt"
+	"io"
 	"net/http"
 )
 
 const (
-	AuthKeyparamName = "auth_key"
+	deeplBaseApiUrl  = "https://api.deepl.com/v2/"
+	authKeyparamName = "auth_key"
 )
 
 // DeeplClient allows easy access to the DeepL API by providing methods for each API function.
@@ -13,4 +16,15 @@ type DeeplClient struct {
 	*http.Client
 	// AuthKey stores the authentication key required to get access DeepL's API.
 	AuthKey []byte
+}
+
+func (client *DeeplClient) doApiFunction(uri string, body io.Reader) (resp *http.Response, err error) {
+	var req *http.Request
+	if req, err = http.NewRequest(http.MethodPost, fmt.Sprintf("%s%s", deeplBaseApiUrl, uri), body); err != nil {
+		return
+	}
+	if resp, err = client.Do(req); err != nil {
+		return nil, err
+	}
+	return
 }
