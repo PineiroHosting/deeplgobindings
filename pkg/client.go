07ee95f8a4e06d93b94b8b221355e3b1f942b69f
mmichaelb <mmichaelb@protonmail.com>
2018-10-29 01:05:27 +0100

    Simplify usage of api function in deepl client

--- pkg/client.go
+++ pkg/client.go
@@ -2,8 +2,9 @@ package deeplgobindings
 
 import (
 	"fmt"
-	"io"
 	"net/http"
+	"net/url"
+	"strings"
 )
 
 const (
@@ -18,11 +19,20 @@ type DeeplClient struct {
 	AuthKey []byte
 }
 
-func (client *DeeplClient) doApiFunction(uri string, body io.Reader) (resp *http.Response, err error) {
+// doApiFunction is an internally used function to execute API functions more easily. The param uri should not begin with
+// a slash character.
+func (client *DeeplClient) doApiFunction(uri string, values *url.Values) (resp *http.Response, err error) {
+	// add authentication header and encode values
+	values.Set(authKeyparamName, string(client.AuthKey))
+	body := strings.NewReader(values.Encode())
+	// create new http request
 	var req *http.Request
 	if req, err = http.NewRequest(http.MethodPost, fmt.Sprintf("%s%s", deeplBaseApiUrl, uri), body); err != nil {
 		return
 	}
+	req.ContentLength = body.Size()
+	// add header to allow the server to identify the POST request
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 	if resp, err = client.Do(req); err != nil {
 		return nil, err
 	}
